fe6c2d10759ee6a67d0fb993950d811679151671
jeangomes01 <jeangomes1567@gmail.com>
2025-11-05 15:22:36 -0300

    feat: add login redirect on logout

--- src/components/navbar/navbar.ts
+++ src/components/navbar/navbar.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
 })
-export class Navbar {
+export class Navbar implements OnInit {
+  constructor(private router: Router) {}
 
+  ngOnInit() {}
+
+  logout() {
+    localStorage.removeItem('token');
+
+    this.router.navigate(['login']);
+  }
 }
