2f5f3f3513a81fb23ebccf59adc47e2bcc3a7be4
jeangomes01 <jeangomes1567@gmail.com>
2025-11-17 14:03:31 -0300

    feat: consume category field in product listing and forms

--- src/app/types/product.interface.ts
+++ src/app/types/product.interface.ts
@@ -1,7 +1,11 @@
+import { IMovement } from './movement.interface';
+
 export interface IProduct {
   id: number;
   name: string;
   description?: string;
   quantity: number;
   price: number;
+  category?: string | null;
+  movements?: IMovement[];
 }
